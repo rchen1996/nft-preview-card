7cb42b082f323e5f1097da8386f7400d26f82b7b
Rebecca Chen <rebecca061996@gmail.com>
2021-11-19 13:08:43 -0500

    add base styling and fonts

---
+++ src/Attribution.js
@@ -0,0 +1,19 @@
+function Attribution() {
+  return (
+    <div class='attribution'>
+      Challenge by{' '}
+      <a
+        href='https://www.frontendmentor.io?ref=challenge'
+        target='_blank'
+        rel='noreferrer'
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{' '}
+      <a href='https://www.linkedin.com/in/rebecca-chen-061996'>Rebecca Chen</a>
+      .
+    </div>
+  );
+}
+
+export default Attribution;
